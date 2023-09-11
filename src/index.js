ff3b097c746a34900dd2ebc2bd09144a2a5ea860
SDudyuk <sdudyuk.o@gmail.com>
2023-09-12 00:15:55 +0300

    add task 3

--- src/index.js
+++ src/index.js
@@ -14,3 +14,26 @@ function padString(str, length, symbol, toLeft) {
 
 console.log('padString: ', "padString('Ivan', 6, '*') => ", padString('Ivan', 6, '*'));
 console.log('padString: ', "padString('Ivan', 6, '*', 'toLeft') => ", padString('Ivan', 6, '*', 'toLeft'));
+
+function camelCase(str, separator) {
+  let camelString = '';
+  let partString = '';
+  let indexSeparator;
+
+  str = str.toLowerCase();
+
+  while (str.includes(separator)) {
+    indexSeparator = str.indexOf(separator);
+    
+    partString = str.slice(0, indexSeparator);
+    camelString = camelString.concat(partString);
+
+    str = str.slice(indexSeparator + 1);
+    str = str.at(0).toUpperCase() + str.slice(1);
+  }
+  camelString = camelString.concat(str);
+
+  return camelString;
+}
+
+console.log(`camelCase: ('Kola*sitro*kVas') => `, camelCase('Kola*sitro*kVas', '*'));
